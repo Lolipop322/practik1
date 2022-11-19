dda4f9d7df220340661ff8efe6ff7f6907bce304
Lolipop322 <amirmuhametzanov067@gmail.com>
2022-11-20 02:53:52 +0300

    minimum

--- 1.py
+++ 1.py
@@ -1,19 +1,8 @@
-#include <iostream>
-#include <vector>
+c = int(input("Количество элементов в массиве: "))
+a = []
+for _ in range(c):
+    a.append(float(input("Число: ")))
+#m = max(map(float, input("Введите числа через пробел: ").split()))
+m = min(a)
+print(f"Максимум в введённом списке: {m}")
 
-using namespace std;
-
-int main(){
-int n;
-cin >> n;
-vector <int> a(n);
-for (int i = 0; i < n; i++) cin >> a[i];
-int b = -1;
-
-for (int i = 0; i < n; i++){
-	b = std::max(b, a[i]);
-}
-
-cout<<b;
-
-}
